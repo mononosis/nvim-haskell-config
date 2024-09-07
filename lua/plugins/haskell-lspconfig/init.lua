f7a0c111996831c98b021e3652820b4146825caf
Your Name <you@example.com>
2024-09-07 09:52:40 +0000

    quick push

--- lua/plugins/haskell-lspconfig/init.lua
+++ lua/plugins/haskell-lspconfig/init.lua
@@ -1,5 +1,10 @@
 local lspconfig = require('lspconfig')
 
+--function CaptureAndNotify()
+  --local output = vim.fn.system('runhaskell main.hs')
+  --vim.notify(output, vim.log.levels.INFO)
+--end
+
 local M = {}
 function M.setup()
   lspconfig.hls.setup {
@@ -13,9 +18,23 @@ function M.setup()
         { noremap = true, silent = true })
       vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>',
         { noremap = true, silent = true })
-      vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', '<cmd>!runghc %<CR>', { noremap = true, silent = true })
+      --vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', ':! runhaskell %<CR>', { noremap = true, silent = true })
+      --vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', ':! runhaskell --ghc-arg=-v0 %<CR>',
+        --{ noremap = true, silent = true })
+      --vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', '<cmd>lua CaptureAndNotify()<CR>',
+        --{ noremap = true, silent = true })
+      vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', ':! runhaskell % | cat<CR>',
+        { noremap = true, silent = true })
+      --vim.api.nvim_buf_set_keymap(buffer, 'n', '<leader>t', ':!runhaskell % > /tmp/haskell_output.txt && cat /tmp/haskell_output.txt<CR>', { noremap = true, silent = true })
     end
     ,
+        -- Haskell-specific settings
+    settings = {
+      haskell = {
+        cabalFormattingProvider = "cabalfmt", -- Formatter for .cabal files
+        formattingProvider = "stylish-haskell", -- You can switch to "fourmolu" here if you prefer
+      }
+    },
   }
 end
 
